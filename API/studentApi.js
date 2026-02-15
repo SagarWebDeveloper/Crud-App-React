f67decd970e8e08d0445168931e5507f287752e2
Sagar Rathor <sagarapple2017@gmail.com>
2026-02-15 12:51:50 +0530

    Implement student API functions using axios

--- API/studentApi.js
+++ API/studentApi.js
@@ -1,1 +1,10 @@
+import axios from "axios";
 
+const API_URL = "http://localhost:3000/students";
+
+export const getStudents = () => axios.get(API_URL);
+export const addStudent = (data) => axios.post(API_URL, data);
+export const deleteStudent = (id) => axios.delete(`${API_URL}/${id}`);
+export const getStudentById = (id) => axios.get(`${API_URL}/${id}`);
+export const updateStudent = (id, data) =>
+  axios.put(`${API_URL}/${id}`, data);
